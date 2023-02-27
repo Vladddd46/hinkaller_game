3c5942c19ff4a0c0002c3d58eeb143d59b213b6c
Petro Shevchenko <shevchenko.p.i@gmail.com>
2023-02-27 12:37:05 +0200

    Added exception catching in main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,28 @@
 #include "Game.hpp"
+#include <iostream>
 
 int main() {
-    Game game;
+    try {
+        Game game;
 
-    // background music
-    sf::Music mainMusic;
-    mainMusic.openFromFile(BACKGROUND_MUSIC);
-    mainMusic.setVolume(50.f);
-    mainMusic.setLoop(true);
-    mainMusic.play();
+        // background music
+        sf::Music mainMusic;
+        mainMusic.openFromFile(BACKGROUND_MUSIC);
+        mainMusic.setVolume(50.f);
+        mainMusic.setLoop(true);
+        mainMusic.play();
 
-    game.loop();
+        game.loop();
+    }
+    catch(std::bad_alloc &err)
+    {
+        std::cerr << "EXCEPTION: There was caught " << err.what() << " exception. Exiting...\n";
+        exit(1);
+    }
+    catch(...)
+    {
+        std::cerr << "EXCEPTION: There was caught unknown exception. Exiting...\n";
+        exit(1);
+    }
     exit(0);
 }
